de29bcb58815310c6012e27528a8579519e4d763
Bigu14 <dawidhajewski4321@gmail.com>
2018-11-20 13:36:01 +0100

    Dodaj pliki projektów.

---
+++ Aplikacja/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplikacja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewData["greeting"] = hour < 17 ? "dzień dobry" : "Dobry wieczór";
+            return View();
+        }
+        
+        [HttpGet]
+        public ViewResult RsvpFrom()
+        {
+            return ();
+        }
+        [HttpGet]
+        public ViewResult RsvpFrom(GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+    }
+}
